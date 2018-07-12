054d26488b809fdcce4779325a055c8c62bf8bba
saltystew <saltystew94@gmail.com>
2018-07-12 00:07:41 -0400

    cleanup

--- title-tagging.py
+++ title-tagging.py
@@ -1,3 +1,10 @@
+import argparse
+import mutagen.mp4
+import progress.bar
+import os
+import sys
+
+
 # Run the script with '--help' to see more information.
 
 
@@ -8,8 +15,7 @@ class bcolors:
 
 
 def arguments():
-    from argparse import ArgumentParser, RawTextHelpFormatter
-    parser = ArgumentParser(description='''
+    parser = argparse.ArgumentParser(description='''
 A script to embed title tags in video files.
 
 Dependencies: Python 3, Mutagen, and Progress
@@ -30,31 +36,29 @@ minus any leading or trailing white spaces.
 An example of the correct format of a file name is below:
 File name: Python 3 Tutorial for Beginners - S01E01 - Why Learn Python?.mp4
 Extracted title tag: Why Learn Python?
-    ''', formatter_class=RawTextHelpFormatter)
+    ''', formatter_class=argparse.RawTextHelpFormatter)
     parser.parse_args()
 
 
 def check_path(directory_path):
-    from os import getcwd
-    from os.path import isdir, join
     while True:
         input_directory = str(input(directory_path)).strip()
         if input_directory.startswith('.\\'):
             input_directory = input_directory.replace('.\\', '', 1)
-        global search_directory
-        search_directory = join((getcwd()), input_directory)
-        if isdir(search_directory):
+        search_directory = os.path.join((os.getcwd()), input_directory)
+        if os.path.isdir(search_directory):
             break
         else:
             print(f'{bcolors.YELLOW}Invalid Directory Path: Please enter a valid directory path.{bcolors.RESET}')
             continue
+    return search_directory
 
 
 def check_titles(correct_tags_dictionary, merged_tags_dictionary):
-    if int(len(correct_tags_dictionary.items())) >= 1:
-        print(f'{len(correct_tags_dictionary.items())} file(s) successfully tagged.')
-    if int(len(merged_tags_dictionary.items())) >= 1:
-        print(f'{bcolors.YELLOW}Found {len(merged_tags_dictionary.items())} file(s) with incorrect title tag{bcolors.RESET}.')
+    if len(correct_tags_dictionary) >= 1:
+        print(f'{len(correct_tags_dictionary)} file(s) successfully tagged.')
+    if len(merged_tags_dictionary) >= 1:
+        print(f'{bcolors.YELLOW}Found {len(merged_tags_dictionary)} file(s) with incorrect title tag{bcolors.RESET}.')
         for file_path, file_title in merged_tags_dictionary.items():
             print(f'{bcolors.YELLOW}Incorrect: Tag for "{file_path}" not successfully set.{bcolors.RESET}')
 
@@ -72,83 +76,83 @@ def confirm_save(confirmation):
             print(f'{bcolors.YELLOW}Invalid Input: Please enter yes or no.{bcolors.RESET}')
 
 
-def fetch_titles(dictionary):
-    from os.path import join
-    for file, root in dictionary.items():
+def fetch_titles(path_walked_dictionary):
+    valid_titles = {}
+    for file, root in path_walked_dictionary.items():
         if ' - ' not in file:
-            print(f'{bcolors.RED}Error: Video File "{join(root, file)}" is not named correctly.{bcolors.RESET}')
+            print(f'{bcolors.RED}Error: Video File "{os.path.join(root, file)}" is not named correctly.{bcolors.RESET}')
             print(f'{bcolors.YELLOW}Run the script with "--help" to see an example of the correct format of a file name.{bcolors.RESET}')
         else:
-            valid_file_path_titles[join(root, file)] = ((file.split(' - '))[-1])[0:-((len(((file.split('.'))[-1]).strip())+1))].strip()
+            valid_titles[os.path.join(root, file)] = ((file.split(' - '))[-1])[0:-((len(((file.split('.'))[-1]).strip())+1))].strip()
+    return valid_titles
 
 
 def handling_tags(correct_tags_dictionary, merged_tags_dictionary):
-    if int(len(correct_tags_dictionary.items())) >= 1:
-        print(f'Found {len(correct_tags_dictionary.items())} file(s) with correct title tag.')
-    if int(len(merged_tags_dictionary.items())) >= 1:
-        print(f'Found {len(merged_tags_dictionary.items())} file(s) with incorrect title tag.')
+    if len(correct_tags_dictionary) >= 1:
+        print(f'Found {len(correct_tags_dictionary)} file(s) with correct title tag.')
+    if len(merged_tags_dictionary) >= 1:
+        print(f'Found {len(merged_tags_dictionary)} file(s) with incorrect title tag.')
         for file_path, file_title in merged_tags_dictionary.items():
             print(f'The title for "{file_path}" would be "{file_title}".')
         return True
-    elif int(len(merged_tags_dictionary.items())) == 0:
+    elif len(merged_tags_dictionary) == 0:
         return False
 
 
 def main():
     arguments()
-    check_path('Which directory would you like to search (recursively)? ')
-    if walk_the_path(search_directory):
-        fetch_titles(path_walked)
-        sort_tags(valid_file_path_titles)
-        if handling_tags(correct_tags, merged_empty_incorrect_tags):
-            if confirm_save('Would you like to update the file(s) as shown above (Y/n)? '):
-                update_title_progress(merged_empty_incorrect_tags)
-                correct_tags.clear()
-                incorrect_tags.clear()
-                empty_tags.clear()
-                merged_empty_incorrect_tags.clear()
-                sort_tags(valid_file_path_titles)
-                check_titles(correct_tags, merged_empty_incorrect_tags)
-    else:
-        print(f'{bcolors.YELLOW}No files found when searching for files ending with ".{extension_limit}".{bcolors.RESET}')
+    search_directory = check_path('Which directory would you like to search (recursively)? ')
+    path_walked = walk_the_path(search_directory)
+    valid_titles = fetch_titles(path_walked)
+    correct_tags, incorrect_tags, empty_tags, merged_tags = sort_tags(valid_titles)
+    if handling_tags(correct_tags, merged_tags):
+        if confirm_save('Would you like to update the file(s) as shown above (Y/n)? '):
+            update_title_progress(merged_tags)
+            correct_tags.clear()
+            incorrect_tags.clear()
+            empty_tags.clear()
+            merged_tags.clear()
+            correct_tags, incorrect_tags, empty_tags, merged_tags = sort_tags(valid_titles)
+            check_titles(correct_tags, merged_tags)
     terminate()
 
 
-def sort_tags(dictionary):
-    from mutagen.mp4 import MP4
-    for file_path, file_title in dictionary.items():
-        video_file = MP4(file_path)
-        if '©nam' in video_file.keys():
-            if file_title in video_file.get(key='©nam'):
-                correct_tags[file_path] = file_title
-            elif file_title not in video_file.get(key='©nam'):
-                incorrect_tags[file_path] = file_title
+def sort_tags(valid_titles_dictionary):
+    correct = {}
+    incorrect = {}
+    empty = {}
+    merged = {}
+    for file_path, file_title in valid_titles_dictionary.items():
+        video_file = mutagen.mp4.MP4(file_path)
+        if '©nam' in video_file:
+            if file_title in video_file.get('©nam'):
+                correct[file_path] = file_title
+            else:
+                incorrect[file_path] = file_title
         else:
-            empty_tags[file_path] = file_title
-    merged_empty_incorrect_tags.update(empty_tags)
-    merged_empty_incorrect_tags.update(incorrect_tags)
+            empty[file_path] = file_title
+    merged.update(empty)
+    merged.update(incorrect)
+    return correct, incorrect, empty, merged
 
 
 def terminate():
-    from sys import exit
     print('Exiting script.')
-    exit()
+    sys.exit()
 
 
-def update_title(dictionary):
-    from mutagen.mp4 import MP4
-    for file_path, file_title in dictionary.items():
-        video_file = MP4(file_path)
+def update_title(tags_dictionary):
+    for file_path, file_title in tags_dictionary.items():
+        video_file = mutagen.mp4.MP4(file_path)
         video_file['©nam'] = [file_title]
         video_file.save()
 
 
-def update_title_progress(dictionary):
-    from progress.bar import FillingSquaresBar
-    count = int(len(dictionary.items()))
-    bar = FillingSquaresBar('Processing', max=count, suffix='%(index)d/%(max)d - %(percent).1f%%')
-    for file in range(count):
-        update_title(dictionary)
+def update_title_progress(tags_dictionary):
+    count = len(tags_dictionary)
+    bar = progress.bar.FillingSquaresBar('Processing', max=count, suffix='%(index)d/%(max)d - %(percent).1f%%')
+    for tag in range(count):
+        update_title(tags_dictionary)
         count -= 1
         bar.next()
     bar.finish()
@@ -157,33 +161,28 @@ def update_title_progress(dictionary):
 def valid_extension(extension):
     valid_extension_list = ['mp4']
     while True:
-        global extension_limit
         extension_limit = str(input(extension)).strip()
         if extension_limit in valid_extension_list:
             break
         else:
             print(f'{bcolors.YELLOW}Invalid Input: Please enter one of the following supported extensions {valid_extension_list}.{bcolors.RESET}')
             continue
+    return extension_limit
 
 
 def walk_the_path(valid_directory_path):
-    from os import walk
-    valid_extension('What file extension would you like to use to narrow the search? ')
-    for root, subdirs, files in walk(valid_directory_path):
+    path_walked = {}
+    extension_limit = valid_extension('What file extension would you like to use to narrow the search? ')
+    for root, subdirs, files in os.walk(valid_directory_path):
         for file in files:
             if file.endswith(extension_limit):
                 path_walked[file] = root
     if len(path_walked) == 0:
-        return False
+        print(f'{bcolors.YELLOW}No files found when searching for files ending with ".{extension_limit}".{bcolors.RESET}')
+        terminate()
     elif len(path_walked) >= 1:
-        return True
+        return path_walked
 
 
 if __name__ == '__main__':
-    correct_tags = {}
-    empty_tags = {}
-    incorrect_tags = {}
-    merged_empty_incorrect_tags = {}
-    path_walked = {}
-    valid_file_path_titles = {}
     main()
